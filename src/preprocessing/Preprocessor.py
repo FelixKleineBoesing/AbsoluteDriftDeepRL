6a20bbafaf9bbfb9c5fd4b1df48791bcd7a671fb
Felix Kleine Bösing <felix.boesing@t-online.de>
2019-04-30 23:39:27 +0200

    implement struct for agentpreprocessor

--- src/preprocessing/Preprocessor.py
+++ src/preprocessing/Preprocessor.py
@@ -1,25 +1,61 @@
 import abc
+import numpy as np
+from scipy.misc import imresize
 
 
 class Preprocessor(abc.ABC):
 
-    def __init__(self):
+    def __init__(self, img_size: tuple):
         """
         takes image and preprocess this for the agent /learner/reward catcher
         """
+        self.img_size = img_size
+
     @abc.abstractmethod
-    def preprocess(self):
+    def preprocess(self, img: np.ndarray):
         pass
+
+    @abc.abstractmethod
+    def _crop_image(self, img: np.ndarray):
+        pass
+
+    def _normalize_img(self, img: np.ndarray):
+        img = img.astype('float32') / 255.
+        return img
+
+    def _convert_to_grey(self, img: np.ndarray):
+        img = img.mean(-1, keepdims=True)
+        return img
+
+    def _resize_img(self, img: np.ndarray):
+        # resize img to specified range
+        img = imresize(img, self.img_size)
+        return img
 
 
 class RewardPreprocessor(Preprocessor):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, img_size: tuple):
+        super().__init__(img_size=img_size)
+
+    def preprocess(self, img: np.ndarray):
         pass
+
 
 class AgentPreprocessor(Preprocessor):
 
-    def __init__(self):
-        super().__init__()
-        pass
+    def __init__(self, img_size: tuple):
+        super().__init__(img_size=img_size)
+
+    def preprocess(self, img: np.ndarray):
+        img = self._crop_image(img)
+        img = self._resize_img(img)
+        img = self._convert_to_grey(img)
+        img = self._normalize_img(img)
+        return img
+
+
+    def _crop_image(self, img: np.ndarray):
+        # slice img here (crop top/bottom,  left/right edgeds which are not really neceessary
+        img = img[:, :, :]
+        return img
