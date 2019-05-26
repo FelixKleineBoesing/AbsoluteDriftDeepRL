fc78e84bd31d14b9e1f1c14c32a6257207161085
Felix Kleine Bösing <felix.boesing@t-online.de>
2019-05-26 22:07:29 +0200

    update recorder

--- src/recorder/Recorder.py
+++ src/recorder/Recorder.py
@@ -1,7 +1,9 @@
-# TODO embedd pyscreenshot to take screenshots during the game
-import pyscreenshot
-#TODO embedd keyboard for logging of keyboard actions
+import pyscreenshot as pysc
 import keyboard
+import datetime
+import time
+import pandas as pd
+import multiprocessing as mp
 
 
 class Recorder:
@@ -13,4 +15,35 @@ class Recorder:
         self.save_path = save_path
 
     def record(self):
+        # TODO start image recording in new thread, since keylogging blocks
+        queue = mp.Queue()
+
         pass
+
+    def _record_images(self, queue: mp.Queue):
+        # TODO STopped should be set to true if space + Esc is send
+        stopped = False
+        images = []
+        timestamps = []
+        while not stopped:
+            time_stamp = datetime.datetime.now()
+            image = pysc.grab()
+            images.append(image)
+            timestamps.append(time_stamp)
+        queue.put(images)
+        queue.put(timestamps)
+
+    def _record_key_strokes(self):
+        records = keyboard.record(until="space+esc")
+        times = []
+        keys = []
+        for record in records:
+            times.append(record.time)
+            keys.append(record.name)
+        key_records = pd.DataFrame({"keys": keys, "times": times})
+
+        return key_records
+
+if __name__ == "__main__":
+    recorder = Recorder()
+    recorder._record_key_strokes()
