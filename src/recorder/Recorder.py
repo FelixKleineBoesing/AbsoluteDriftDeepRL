29a5dcfb1f86d894614c3429c4ad7b8d8c432750
Felix Kleine Bösing <felix.boesing@t-online.de>
2019-06-10 14:16:01 +0200

    update Recorder

--- src/recorder/Recorder.py
+++ src/recorder/Recorder.py
@@ -3,7 +3,11 @@ import keyboard
 import datetime
 import time
 import pandas as pd
+import os
 import multiprocessing as mp
+import json
+
+lock = mp.Lock()
 
 
 class Recorder:
@@ -11,40 +15,56 @@ class Recorder:
     takes game object and records states and actions
     """
 
-    def __init__(self, save_path: str = "data/records/"):
+    def __init__(self, save_path: str = "../../data/records/"):
         self.save_path = save_path
 
     def record(self):
-        # TODO start image recording in new thread, since keylogging blocks
-        queue = mp.Queue()
+        managed_dict = mp.Manager().dict()
+        managed_dict["running"] = True
+        image_process = mp.Process(target=self._record_images, args=(managed_dict, self.save_path))
+        key_process = mp.Process(target=self._record_key_strokes, args=(managed_dict, self.save_path))
 
-        pass
+        image_process.start()
+        key_process.start()
 
-    def _record_images(self, queue: mp.Queue):
-        # TODO STopped should be set to true if space + Esc is send
-        stopped = False
-        images = []
-        timestamps = []
-        while not stopped:
-            time_stamp = datetime.datetime.now()
-            image = pysc.grab()
-            images.append(image)
-            timestamps.append(time_stamp)
-        queue.put(images)
-        queue.put(timestamps)
+    def _record_images(self, managed_dict, save_path: str):
+        try:
+            stopped = False
+            images = []
+            timestamps = []
+            while not stopped:
+                time_stamp = datetime.datetime.now()
+                image = pysc.grab()
+                images.append(image)
+                timestamps.append(time_stamp)
+                print(type(image))
+                running = managed_dict["running"]
+                print(running)
+                print(type(managed_dict["running"]))
+                if not bool(managed_dict["running"]):
+                    raise KeyboardInterrupt("Execution was interrupted")
+        finally:
+            if len(images) > 0:
+                print(os.getcwd())
+                with open(save_path + "images.json", "w") as f:
+                    json.dump({"images": images, "timestamps": timestamps}, f)
 
-    def _record_key_strokes(self):
-        records = keyboard.record(until="space+esc")
-        times = []
-        keys = []
-        for record in records:
-            times.append(record.time)
-            keys.append(record.name)
-        key_records = pd.DataFrame({"keys": keys, "times": times})
-
-        return key_records
+    def _record_key_strokes(self, managed_dict, save_path: str):
+        try:
+            records = keyboard.record(until="space+esc")
+        finally:
+            if len(records) > 0:
+                times = []
+                keys = []
+                for record in records:
+                    times.append(record.time)
+                    keys.append(record.name)
+                key_records = {"keys": keys, "times": times}
+                print(os.getcwd())
+                with open(save_path + "keys.json", "w") as f:
+                    json.dump(key_records,f)
 
 
 if __name__ == "__main__":
     recorder = Recorder()
-    recorder._record_key_strokes()
+    recorder.record()
